5d698e1f89fbc89f30becb157d00576e3c8a0947
Emma <emma.upton.95@gmail.com>
2018-02-21 23:21:17 +0000

    updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Prime Number REST API implemented with AKKA-HTTP
 This application exposes an API to retrieve a JSON-formatted list of prime numbers that exist before and inclusive of a given integer.
 The prime numbers list can be retrieved through two different algorithms.
+
+## To run
+This application is currently configured to run on port 8002. To run this application you need SBT installed, and then when within the directory of the repository enter the following command:
+```sbt run```
+
+## API
+
+### GET /prime/:algorithm/:limit
+
+Takes in two string parameters. The first indicates the algorithm to be used for the computation, the second the
+limit at which primes should stop being computed.
+
+Supported algorithm strings:
+
+`algoA` - computers the primes by filtering a pre-computed (i.e non-lazily evaluted) list (of limit length) with a predicate that determines whether an element is a prime or not
+
+`algoB` - takes from an infinite stream of prime numbers (i.e lazily evaluated) a finite number of primes by filtering with a predicate where the elements must be less than the limit supplied
+
+Responds with a JSON document containing a list of the prime numbers computed.
+
+Example response:
+```json
+    "primes": ["2",
+               "3",
+               "5",
+               "7",
+               "13"]
+```
+Error codes:
++ 400 BadRequest this can be triggered by either supplying an invalid algorithm string or supplying a very large limit
--- src/app/Routes.scala
+++ src/app/Routes.scala
@@ -1,15 +1,17 @@
 package app
 
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Route, StandardRoute}
 import akka.http.scaladsl.server.RouteConcatenation._enhanceRouteWithConcatenation
 import akka.http.scaladsl.server.directives.PathDirectives._
 import akka.http.scaladsl.server.directives.MethodDirectives._
 import akka.http.scaladsl.server.directives.RouteDirectives._
-import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.StatusCodes.{BadRequest, OK}
 import com.google.inject.Inject
 import model.PrimeList
 import util.Constants._
 import util.ImplicitJsonConversions._
+
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by emma on 19/02/2018.
@@ -23,15 +25,7 @@ class Routes @Inject()(appConfig: AppConfig){
           path(Remaining) {
             n: String =>
               get {
-                if(n.toInt <= appConfig.requestLimit) {
-                  algorithm match {
-                    case ALGORITHM_A => complete(OK, PrimeList(AlgorithmA.obtainPrimesUpToN(n.toInt)))
-                    case ALGORITHM_B => complete(OK, PrimeList(AlgorithmB.obtainPrimesUpToN(n.toInt)))
-                    case _ => complete(BadRequest)
-                  }
-                }
-                else {
-                  complete(BadRequest, s"Invalid request - max value of ${appConfig.requestLimit} for n")
+                processRequest(algorithm, n)
                 }
               }
           }
@@ -41,6 +35,22 @@ class Routes @Inject()(appConfig: AppConfig){
         complete(OK)
       }
     }
-  }
 
+  def processRequest(algorithm: String, n: String): StandardRoute = {
+    Try(
+      if(n.toInt <= appConfig.requestLimit) {
+        algorithm match {
+          case ALGORITHM_A => complete(OK, PrimeList(AlgorithmA.obtainPrimesUpToN(n.toInt)))
+          case ALGORITHM_B => complete(OK, PrimeList(AlgorithmB.obtainPrimesUpToN(n.toInt)))
+          case _ => complete(BadRequest, "Invalid request - supply a valid algorithm")
+        }
+      }
+      else {
+        complete(BadRequest, s"Invalid request - max value of ${appConfig.requestLimit} for n")
+      }
+    ) match {
+      case Success(route) => route
+      case Failure(exception: NumberFormatException) => complete(BadRequest, "Invalid request - supply a valid number")
+    }
+  }
 }
